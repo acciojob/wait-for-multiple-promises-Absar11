dd9f4f06778ddd76ddb492d6bf9bd104cb371281
Absar Ahmad <121477562+Absar11@users.noreply.github.com>
2025-07-10 22:16:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 //your JS code here. If required.
+let output = document.getElementById('output');
+
+let promise1Start = Date.now();
+let promise1 = new Promise((resolve) => {
+	setTimeout(()=>{
+		let time = ((Date.now() - promise1Start) / 1000).toFixed(2);
+		resolve({ name: 'Promise 1', time });
+	}, 2000)
+})
+
+let promise2Start = Date.now();
+let promise2 = new Promise((resolve) => {
+	setTimeout(()=>{
+		let time = ((Date.now() - promise2Start) / 1000).toFixed(2);
+		resolve({ name: 'Promise 2', time });
+	}, 1000)
+})
+
+let promise3Start = Date.now();
+let promise3 = new Promise((resolve) => {
+	setTimeout(()=>{
+		let time = ((Date.now() - promise3Start) / 1000).toFixed(2);
+		resolve({ name: 'Promise 3', time });
+	}, 3000)
+})
+
+
+let timeStart = Date.now();
+
+Promise.all([promise1, promise2, promise3])
+	.then(results => {
+		output.innerHTML = ''; 
+		results.forEach(p => {
+			let row = document.createElement('tr');
+			row.innerHTML = `<td>${p.name}</td><td>${p.time}</td>`;
+			output.appendChild(row);
+		});
+		let timeTaken = ((Date.now() - timeStart) / 1000);
+		let row = document.createElement('tr');
+		row.innerHTML = `<td><strong>Total</strong></td>
+						<td>${timeTaken}</td>`;
+		output.appendChild(row);
+});
+
+
+
